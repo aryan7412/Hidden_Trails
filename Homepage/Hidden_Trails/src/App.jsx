c0da8d0ac81b2cb7a1d32fc6bc1528452d814859
aryan7412 <aryan7412ss@gmail.com>
2024-12-18 20:17:41 +0530

    day1

---
+++ Homepage/Hidden_Trails/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl'>Kaam start karo</h1>
+    </>
+  )
+}
+
+export default App
